3d6ea300761cece95fec4fdb09cb409cb85f5840
Daniel Vieira <danielviei@hotmail.com>
2022-06-19 01:26:15 -0400

    feat: added logs app

---
+++ images_processing/images_processing/apps/logs/models.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+from django.db import models
+
+class Logs(models.Model):
+    date = models.DateTimeField(default=datetime.now())
+    error_code = models.CharField(max_length=10)
+    description = models.TextField()
--- images_processing/images_processing/settings.py
+++ images_processing/images_processing/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'images_processing.apps.jobs.apps.JobsConfig'
+    'images_processing.apps.jobs.apps.JobsConfig',
+    'images_processing.apps.logs.apps.LogsConfig',
 ]
 
 MIDDLEWARE = [
