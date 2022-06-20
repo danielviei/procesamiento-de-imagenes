fafca0887e5c50e44e43b062c639e0c9281af1e0
Daniel Vieira <danielviei@hotmail.com>
2022-06-20 09:40:33 -0400

    feat: added get one job

--- images_processing/images_processing/apps/logs/models.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-from django.db import models
-
-class Logs(models.Model):
-    date = models.DateTimeField(default=datetime.now)
-    error_code = models.CharField(max_length=10)
-    description = models.TextField()
--- images_processing/images_processing/apps/logs/views.py
+++ images_processing/images_processing/apps/logs/views.py
@@ -1,11 +1,28 @@
 from django_db_logger.models import StatusLog
-from rest_framework import viewsets
+from rest_framework import generics
 
 from .serializers import LogsSerializer
 
-class LogsViewSet(viewsets.ModelViewSet):
+class LogsListAPIView(generics.ListAPIView):
     """
-    API endpoint that allows Logs to be viewed or edited.
+    API endpoint that allows Logs to be viewed.
+    """
+    serializer_class = LogsSerializer
+
+    def get_queryset(self):
+        queryset = StatusLog.objects.all()
+        min_date = self.request.query_params.get('min_date')
+        if min_date is not None:
+            queryset = queryset.filter(create_datetime__gte=min_date)
+        max_date = self.request.query_params.get('max_date')
+        if max_date is not None:
+            queryset = queryset.filter(create_datetime__lte=max_date)
+        return queryset
+
+class LogsRetrieveAPIView(generics.RetrieveAPIView):
+    """
+    API endpoint that returns a Logs object.
     """
     queryset = StatusLog.objects.all()
     serializer_class = LogsSerializer
+    lookup_field = 'id'
